fc25bf8c5178b21353c21ecb178bfab4cd9e6ba6
fariraimasocha <114150009+fariraimasocha@users.noreply.github.com>
2022-12-05 16:00:28 +0200

    noad

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Source;
+use App\Scraper\Scraper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    private Scraper $scraper;
+
+    public function __construct(Scraper $scraper)
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        $this->scraper = $scraper;
+    }
+
+    /**
+     * @Route("/fetch/{id}", name="fetch")
+     */
+    public function fetch(Source $source)
+    {
+        $posts = $this->scraper->scrap($source);
+
+        return $this->json($posts->toArray());
     }
 }
