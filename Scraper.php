348f873dec664fefa14c365e01f96700581c63fd
fariraimasocha <114150009+fariraimasocha@users.noreply.github.com>
2022-12-05 16:27:01 +0200

    meek

--- Scraper.php
+++ Scraper.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\Panther\Client;
+use App\Controller\Admin\HomeController;
 
 class Scraper
 {
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,29 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Source;
-use App\Scraper\Scraper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-
 
 class HomeController extends AbstractController
 {
-    private Scraper $scraper;
-
-    public function __construct(Scraper $scraper)
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
     {
-        $this->scraper = $scraper;
-    }
-
-    /**
-     * @Route("/fetch/{id}", name="fetch")
-     */
-    public function fetch(Source $source)
-    {
-        $posts = $this->scraper->scrap($source);
-
-        return $this->json($posts->toArray());
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
     }
 }
